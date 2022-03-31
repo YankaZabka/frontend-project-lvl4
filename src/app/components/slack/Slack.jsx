9e7096c15db2c460382f21f309fd351d29e42539
YankaZabka <yanka.zanka@gmail.com>
2022-03-31 14:25:58 +0300

    Add data fetching to Slack.jsx useEffect

--- src/app/components/slack/Slack.jsx
+++ src/app/components/slack/Slack.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import ChannelsList from './channels/ChannelsList.jsx';
 import MessagesList from './messages/MessagesList.jsx';
 import SlackContainer from './SlackContainer.jsx';
 
 function Slack() {
+  useEffect(() => {
+    const userInfo = localStorage.getItem('user');
+    const parsedInfo = JSON.parse(userInfo);
+    const { token } = parsedInfo;
+    console.log(token);
+
+    const fetch = async () => {
+      const response = await axios.get('/api/v1/data', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data);
+    };
+
+    fetch();
+  });
+
   return (
     <SlackContainer>
 
