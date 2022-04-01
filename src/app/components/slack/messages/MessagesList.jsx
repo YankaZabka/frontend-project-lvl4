acebd07f834d503df99550d64a07de8b89e4d113
YankaZabka <yanka.zanka@gmail.com>
2022-04-01 14:34:12 +0300

    Provide values to Messageslist.jsx

--- src/app/components/slack/messages/MessagesList.jsx
+++ src/app/components/slack/messages/MessagesList.jsx
@@ -3,15 +3,21 @@ import MessageItem from './MessageItem.jsx';
 import MessageForm from './MessageForm.jsx';
 import MessagesHeader from './MessagesHeader.jsx';
 import MessagesContainer from './MessagesContainer.jsx';
+import { selectors } from "../../../slices/messagesSlice";
+import {useSelector} from "react-redux";
 
 function MessagesList() {
+    const currentChannel = useSelector((state) => state.channels.selectedChannel)
+    const messages = useSelector(selectors.selectAll).filter(item => item.channelId === currentChannel)
+
   return (
     <MessagesContainer>
       <MessagesHeader />
 
       <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-        <MessageItem author="Zyrael" text="hgfhfdhfg" />
-        <MessageItem author="admin" text="d" />
+          {messages && messages.map(({author, text, id}) => {
+              return <MessageItem key={id} author={author} text={text} />
+          })}
       </div>
 
       <MessageForm />
