5421b1219836b41ce9f8e6745aae935c6b117bfd
YankaZabka <yanka.zanka@gmail.com>
2022-04-01 14:33:57 +0300

    Provide values to MessagesHeader.jsx

--- src/app/components/slack/messages/MessagesHeader.jsx
+++ src/app/components/slack/messages/MessagesHeader.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {selectors} from "../../../slices/channelsSlice.js";
 
 function MessagesHeader() {
-  return (
+    const currentChannelId = useSelector((state) => state.channels.selectedChannel)
+    const currentChannel = useSelector((state) => selectors.selectById(state, currentChannelId))
+    console.log(currentChannel)
+
+
+    return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
-      <p className="m-0"><b># general</b></p>
-      <span className="text-muted">
-        77 сообщений
-      </span>
+        {currentChannel && <>
+            <p className="m-0"><b># {currentChannel.name}</b></p>
+            <span className="text-muted">77 сообщений</span>
+        </>}
+
     </div>
   );
 }
