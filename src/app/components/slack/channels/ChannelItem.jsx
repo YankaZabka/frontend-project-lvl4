72079380bb967d9e4a6179d42147534f891c429a
YankaZabka <yanka.zanka@gmail.com>
2022-04-01 14:31:20 +0300

    Add 'removable' condition to ChannelItem render method

--- src/app/components/slack/channels/ChannelItem.jsx
+++ src/app/components/slack/channels/ChannelItem.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
-function ChannelItem({ name }) {
+function ChannelItem({ name, id, removable }) {
+    const currentId = useSelector((state) => state.channels.selectedChannel)
+    const isSelected = currentId === id ? "btn-secondary" : ""
+
   return (
     <li className="nav-item w-100">
-      <button type="button" className="w-100 rounded-0 text-start btn">
-        <span
-          className="me-1"
-        >
-          #
-        </span>
-        {name}
-      </button>
+        {removable
+            ? <div role="group" className="d-flex dropdown btn-group">
+                <button type="button" className={`w-100 rounded-0 text-start text-truncate btn ${isSelected}`}><span
+                    className="me-1">#</span>{name}
+                </button>
+                <button type="button" id="react-aria8038969833-1"
+                        aria-expanded="false"
+                        className={`flex-grow-0 dropdown-toggle dropdown-toggle-split btn ${isSelected}`}
+                >
+                    <span className="visually-hidden">Управление каналом</span>
+                </button>
+            </div>
+            : <button type="button" className={`w-100 rounded-0 text-start btn ${isSelected}`}>
+                <span className="me-1">#</span>
+                {name}
+            </button>
+        }
     </li>
   );
 }
