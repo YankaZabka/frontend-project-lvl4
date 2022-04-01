0d826919ddc0fa9b3ef04e5db84ef30618a0acc8
YankaZabka <yanka.zanka@gmail.com>
2022-04-01 15:13:08 +0300

    Add responsiveness to MessageForm UI

--- src/app/components/slack/messages/MessageForm.jsx
+++ src/app/components/slack/messages/MessageForm.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import useSocket from '../../../hooks/useSocket';
 
 function MessageForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const inputEl = useRef();
+
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
+
   const socket = useSocket();
   const channelId = useSelector((state) => state.channels.selectedChannel);
   const author = JSON.parse(localStorage.getItem('user')).username;
@@ -13,8 +20,13 @@ function MessageForm() {
       text: '',
     },
     onSubmit: ({ text }, { resetForm }) => {
-      socket.emit('newMessage', { text, channelId, author });
-      resetForm();
+      setIsLoading(true);
+
+      socket.emit('newMessage', { text, channelId, author }, () => {
+        setIsLoading(false);
+        resetForm();
+        inputEl.current.focus();
+      });
     },
   });
 
@@ -29,23 +41,35 @@ function MessageForm() {
             aria-label="Новое сообщение"
             placeholder="Введите сообщение..."
             className="border-0 p-0 ps-2 form-control"
+            ref={inputEl}
             value={formik.values.text}
             onChange={formik.handleChange}
           />
-          <button type="submit" disabled={formik.values.text === ''} className="btn btn-group-vertical">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              width="20"
-              height="20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-              />
-            </svg>
-            <span className="visually-hidden">Отправить</span>
+          <button type="submit" disabled={formik.values.text === '' || isLoading} className="btn btn-group-vertical">
+            {!isLoading
+              ? (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                    />
+                  </svg>
+                  <span className="visually-hidden">Отправить</span>
+                </>
+              )
+              : (
+                <div className="spinner-border spinner-border-sm" role="status">
+                  <span className="visually-hidden">Загрузка...</span>
+                </div>
+              )}
+
           </button>
         </div>
       </form>
