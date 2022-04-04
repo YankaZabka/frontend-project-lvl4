73bce0a978bcd1178a46e0be5e2481a9deed28fa
YankaZabka <yanka.zanka@gmail.com>
2022-04-04 11:18:42 +0300

    Add socket subscription on 'renameChannel'

--- src/app/contexts/providers/socketProvider.jsx
+++ src/app/contexts/providers/socketProvider.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import SocketContext from '../socket.js';
 import { addMessage } from '../../slices/messagesSlice';
-import { addChannel, removeChannel } from '../../slices/channelsSlice';
+import { addChannel, removeChannel, renameChannel } from '../../slices/channelsSlice';
 
 const socketProvider = ({ children }) => {
   const socket = io();
@@ -18,14 +18,14 @@ const socketProvider = ({ children }) => {
   socket.on('removeChannel', ({ id }) => {
     dispatch(removeChannel(id));
   });
-  // socket.on('renameChannel', (channel) => {
-  //     store.dispatch(renameChannel({
-  //         id: channel.id,
-  //         changes: {
-  //             name: channel.name,
-  //         },
-  //     }));
-  // });
+  socket.on('renameChannel', (channel) => {
+    dispatch(renameChannel({
+      id: channel.id,
+      changes: {
+        name: channel.name,
+      },
+    }));
+  });
 
   return (
     <SocketContext.Provider value={socket}>
