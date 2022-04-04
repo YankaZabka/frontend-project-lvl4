e93fb2b3a91bebd58b90fecd2d589eef08e34396
YankaZabka <yanka.zanka@gmail.com>
2022-04-04 11:26:17 +0300

    Add 'messagesAmount' value to MessagesHeader.jsx

--- src/app/components/slack/messages/MessagesHeader.jsx
+++ src/app/components/slack/messages/MessagesHeader.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectors } from '../../../slices/channelsSlice.js';
+import { selectors as channelSelectors } from '../../../slices/channelsSlice.js';
+import { selectors as messageSelectors } from '../../../slices/messagesSlice.js';
 
 function MessagesHeader() {
   const currentChannelId = useSelector((state) => state.channels.selectedChannel);
-  const currentChannel = useSelector((state) => selectors.selectById(state, currentChannelId));
+  const currentChannel = useSelector((state) => channelSelectors
+    .selectById(state, currentChannelId));
+  const messagesAmount = useSelector(messageSelectors.selectAll)
+    .filter((item) => item.channelId === currentChannelId).length;
 
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
@@ -16,7 +20,11 @@ function MessagesHeader() {
               {currentChannel.name}
             </b>
           </p>
-          <span className="text-muted">77 сообщений</span>
+          <span className="text-muted">
+            {messagesAmount}
+            {' '}
+            сообщений
+          </span>
         </>
       )}
 
